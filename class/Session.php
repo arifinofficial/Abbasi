948f3ed7cccc163136c9f331a81f8e0d37ac33f7
Arifin Nurmardiansyah <arifinofficial30@gmail.com>
2018-06-28 02:27:11 +0800

    add class session

---
+++ class/Session.php
@@ -0,0 +1,14 @@
+<?php
+
+class Session
+{
+    public static function set($name, $value)
+    {
+        return $_SESSION[$name] = $value;
+    }
+
+    public static function get($name)
+    {
+        return $_SESSION[$name];
+    }
+}
